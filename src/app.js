dad678267d99d6741815c18712484aca6d611f75
Jonas Schumacher <jonas@Teelkes-MacBook-Pro.local>
2016-12-03 17:41:18 +0100

    Some design stuff

--- src/app.js
+++ src/app.js
@@ -8,6 +8,8 @@ import jetpack from 'fs-jetpack'; // module loaded from npm
 import { greet } from './hello_world/hello_world'; // code authored by you in this project
 import env from './env';
 import { db } from './db/db';
+window.$ = window.jQuery = require('jquery');
+const { BrowserWindow } = require('electron');
 
 console.log('Loaded environment variables:', env);
 
@@ -19,8 +21,26 @@ var appDir = jetpack.cwd(app.getAppPath());
 console.log('The author of this app is:', appDir.read('package.json', 'json').author);
 
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('greet').innerHTML = greet();
-    document.getElementById('platform-info').innerHTML = os.platform();
-    document.getElementById('env-name').innerHTML = env.name;
+  // let child = new BrowserWindow({parent: top, modal: true, show: false})
+  // child.loadURL('https://github.com')
+  // child.once('ready-to-show', () => {
+  //   child.show()
+  // });
+    // document.getElementById('greet').innerHTML = greet();
+    // document.getElementById('platform-info').innerHTML = os.platform();
+    // document.getElementById('env-name').innerHTML = env.name;
     db();
+    document.getElementById('checkButton').addEventListener('click', function() {
+      $('.container').html('<div class="spinner">\
+            <div class="rect1"></div>\
+            <div class="rect2"></div>\
+            <div class="rect3"></div>\
+            <div class="rect4"></div>\
+            <div class="rect5"></div>\
+          </div>'
+      );
+      window.setTimeout(function () {
+        $('.container').html('<p>Finished!</p>')
+      }, 5000);
+    });
 });
